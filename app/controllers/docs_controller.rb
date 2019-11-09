44e3a6a8f3faa218c01841c61e554e6f824537ca
nuke <rochadcmarcos@gmail.com>
2019-11-09 03:49:11 -0300

    edit and destroy docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -28,9 +28,16 @@ class DocsController < ApplicationController
 
     # use the edit view to modify the database
     def update
+        if @doc.update(doc_params)
+            redirect_to @doc 
+        else 
+            render 'edit'
+        end
     end
 
     def destroy
+        @doc.destroy
+        redirect_to docs_path
     end
 
     private
