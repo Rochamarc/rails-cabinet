c8e31114f8dfa7b36481ae23077a9285f9ce0c81
nuke <rochadcmarcos@gmail.com>
2019-11-09 02:43:09 -0300

    Doc controller and route

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,34 @@
+class DocsController < ApplicationController
+
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    # use the new view to create a new doc in the db
+    def create
+    end
+
+    def edit
+    end
+
+    # use the edit view to modify the database
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+        def find_doc
+        end
+
+        def doc_params
+        end
+    
+end
