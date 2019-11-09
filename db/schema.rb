d21648c5fba5502f8a0cb20cf8db488ac39d4976
nuke <rochadcmarcos@gmail.com>
2019-11-09 04:03:44 -0300

    Association user and files

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_065324) do
+ActiveRecord::Schema.define(version: 2019_11_09_065813) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id" #rails generate migration add_user_id_to_docs user_id:integer
+
   end
 
   create_table "users", force: :cascade do |t|
